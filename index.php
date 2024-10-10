bd7924b530da33a3666d52fb0d8e532459ec0160
Diego Selva <diegoselva92@gmail.com>
2024-10-10 11:09:18 -0300

    logo

--- index.php
+++ index.php
@@ -9,9 +9,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="img/logo_accesibilidad_ok.png">
     <link rel="stylesheet" href="css/estilos.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="icon" type="image/png" href="img/logo_accesibilidad_ok.png">
     <title>Dirección de Accesibilidad</title>
 </head>
 <body>
