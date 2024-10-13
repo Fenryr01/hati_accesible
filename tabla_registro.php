7c8265ed020833792b2f01721a5b6e3bd8168b73
Diego Selva <diegoselva92@gmail.com>
2024-10-12 21:27:19 -0300

    permisos

--- tabla_registro.php
+++ tabla_registro.php
@@ -1,5 +1,16 @@
-<?php include("navbar.php"); ?>
+<?php include("navbar.php"); 
+    // Verificar si el usuario está autenticado
+    if (!isset($_SESSION['username'])) {
+        header("Location: index.php"); // Redirigir al inicio si no está autenticado
+        exit;
+    }
 
+    // Verificar permisos específicos
+    if (!isset($_SESSION['permisos']['ver_tablas'])) {
+        header("Location: index.php"); // Redirigir si no tiene permiso
+        exit;
+    }
+    ?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
