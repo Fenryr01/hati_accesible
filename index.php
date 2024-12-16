4bf21e52db7f4eaf3339acd4889b32affa9550b5
Diego Selva <diegoselva92@gmail.com>
2024-12-16 12:46:01 -0300

    service worker off

--- index.php
+++ index.php
@@ -98,20 +98,5 @@
     <script src="js/navbar.js"></script>
     <script src="js/footer.js"></script>
 
-    <script>
-        // Registrar el Service Worker
-        if ('serviceWorker' in navigator) {
-            window.addEventListener('load', () => {
-                navigator.serviceWorker
-                    .register('/service-worker.js')
-                    .then((registration) => {
-                        console.log('Service Worker registrado con éxito:', registration);
-                    })
-                    .catch((error) => {
-                        console.error('Error al registrar el Service Worker:', error);
-                    });
-            });
-        }
-    </script>
 </body>
 </html>
